b8ec749592ed184a794a0b560a04ce31cb3d4e80
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-05-25 22:19:05 +0530

    Creating Video controller and routes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,30 +1,33 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-export const verifyJWT = asyncHandler(async(req, _, next) => {
-    try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        
-        // console.log(token);
-        if (!token) {
-            throw new ApiError(401, "Unauthorized request")
-        }
-    
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-    
-        if (!user) {
-            
-            throw new ApiError(401, "Invalid Access Token")
-        }
-    
-        req.user = user;
-        next()
-    } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token")
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    let token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
     }
-    
-})
+
+    // Convert the token to a string if it's not already
+    if (typeof token !== "string") {
+      token = token.toString();
+    }
+
+    const decodedToken = jwt.verify(token, "nahibGkbjS7dhgd4f3nvsd341na5hib6hdsfg78TXCFG9J9_sdfD074jgsd11fvGa2dklhbc");
+
+    const user = await User.findById(decodedToken._id).select("-password -refreshToken");
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error); // Log the error for debugging purposes
+    throw new ApiError(401, error.message || "Invalid access token");
+  }
+});
