8070c2faf788a9cf49503c2c0fd4b3d131c72f2f
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-05-26 23:12:00 +0530

    Creating models

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const subscriptionSchema = new Schema({
     subscriber:{
@@ -13,5 +14,7 @@ const subscriptionSchema = new Schema({
 {
     timestamps:true
 })
+
+subscriptionSchema.plugin(mongooseAggregatePaginate);
 
 export const subscription = mongoose.model("subscription", subscriptionSchema)
